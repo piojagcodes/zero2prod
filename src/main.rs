7f9076e002f1e3b6ff01d056e7c18336bf43f089
piojagcodes <piojag@gmail.com>
2023-08-08 21:46:20 +0200

    Implementing first working actix_web endpoint by myself

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-use actix_web::{web,App,HttpRequest, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}", &name)
+
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-#[tokio::main]
-async fn main() -> std::io::Result<()>  {
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Hello health check")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
